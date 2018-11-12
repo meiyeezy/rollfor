b076020070471eddbb519b632daa1ad96f1fa516
Alice Cheng <alice.cheng@machine99.thinkjam.local>
2018-11-12 14:39:54 +0000

    added custom google fonts and description

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App extends Component {
       <div className="App">
       <div id="logoHolder"><img src={require("./d20.png")} className="logo" alt="dice" /></div>
       <h1>roll for initiative!</h1>
+      <div className="intro">
+      initiative tracker for any tabletop roleplaying game made by <a href="https://github.com/meiyeezy">meiyeezy</a>. 
+      characters are sorted automatically by highest initiative. both enemies and allies can be 'downed' by clicking on them. 
+      source code is <a href="https://github.com/meiyeezy/rollfor">here</a>. roll high!
+      </div>
         <InitiativeList />
       </div>
     );
